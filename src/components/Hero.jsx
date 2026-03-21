e219ea5cfdab93c8c06827752f68b5e43be47be9
Md Shihab Shahriar Rashu <md.shihabshahriarrashu@gmail.com>
2026-03-21 23:41:39 +0600

    feat/add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import playstoreIcon from '../assets/google-play-store-icon.png';
+import appstoreIcon from '../assets/apple-app-store-icon.png';
+import heroImg from '../assets/hero.png';
 
 const Hero = () => {
     return (
@@ -13,11 +16,17 @@ const Hero = () => {
             {/* store buttons */}
             <div className="flex flex-col sm:flex-row items-center justify-center gap-4 mb-20">
                 <button className="flex items-center gap-2 border-2 border-gray-200 px-6 py-3 rounded-md hover:bg-gray-50 transition-all font-semibold text-gray-800">
-                    <img src="/google-play.svg" alt="Play Store" />
+                    <img src={playstoreIcon} alt="Play Store" className="w-6" />
                     Google Play</button>
                 <button className="flex items-center gap-2 border-2 border-gray-200 px-6 py-3 rounded-md hover:bg-gray-50 transition-all font-semibold text-gray-800">
-                    <img src="/app-store.svg" alt="App Store" />
+                    <img src={appstoreIcon} alt="App Store" className="w-6" />
                     App Store</button>
+            </div>
+
+            <div className="relative max-w-lg mx-auto">
+                <div className="relative z-10">
+                    <img src={heroImg} alt="Hero Image" />
+                </div>
             </div>
         </section>
     )
