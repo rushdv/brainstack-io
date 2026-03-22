06338aba15b336ece411a5fde7e6a907678287c5
Md Shihab Shahriar Rashu <md.shihabshahriarrashu@gmail.com>
2026-03-22 12:49:27 +0600

    feat/add stats section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/navbar"
+import Stats from "./components/Stats";
 function App() {
 
   return (
     <>
       <Navbar />
       <Hero />
+      <Stats />
     </>
   )
 }
